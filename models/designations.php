9bc1c0633601e6f63d61a91ee46e9f0b003385e5
sachin-jangir <99168247+sachin-jangir@users.noreply.github.com>
2022-04-08 09:44:53 +0530

    designation update in progress

--- models/designations.php
+++ models/designations.php
@@ -83,6 +83,87 @@ class DesgModel {
         return 'success';
     }
 
+
+    public function update($data)
+    {
+        $columnList = '(';
+        $params = '(';
+        $paramType = '';
+        $insertData = array();
+        foreach($this->columns as $column) {
+            $columnList .= $column['name'].',';
+            $params .= '?,';
+            $paramType .= $column['type'];
+            if(isset($data[$column['name']]) && $data[$column['name']] !== '') {
+                array_push($insertData, $data[$column['name']]);
+            } else {
+                if($column['required']) {
+                    return 'invalid';
+                } else {
+                    array_push($insertData, null);
+                }
+            }
+        }
+        
+        if($this->updatedAt) {
+            $columnList .= $this->updatedAt['name'].',';
+            $params .= '?,';
+            $paramType .= $this->updatedAt['type'];
+            array_push($insertData, Date('Y-m-d'));
+        }
+
+        if($this->deletedAt) {
+            $columnList .= $this->deletedAt['name'].',';
+            $params .= '?,';
+            $paramType .= $this->deletedAt['type'];
+            array_push($insertData, null);
+        }
+        
+        $paramType .= $this->primaryKey['type'];
+        array_push($insertData, trim($data['id']));
+
+        $columnList[strlen($columnList)-1] = ')';
+        $params[strlen($params)-1] = ')';
+
+        // $query = "UPDATE {$this->table} SET $columnList VALUES $params WHERE {$this->primaryKey['name']} = ?";
+        $query = "UPDATE {$this->table} SET ";
+
+        $arrayLength = sizeof($this->columns)+2;
+
+        for ($i = 0; $i < $arrayLength; $i++) {
+            if ($i == $arrayLength - 1) {
+                $query .= " ".$columnList[$i].", ".$params[$i];
+            }
+            else {
+                $query .= " ".$columnList[$i].", ".$params[$i]." ,";
+            }
+        }
+
+        $query = " WHERE {$this->primaryKey['name']} = ?";
+        
+        
+
+        $result = true;
+        try {
+            $stmt = $this->conn->prepare($query);
+            $stmt->bind_param($paramType, ...$insertData);
+            $result = $stmt->execute();
+        } catch (Exception $e)  {
+            if($this->conn->errno == 1062) {
+                return 'duplicate';
+            }
+            return 'error';
+        }
+        $stmt->close();
+        if(!$result) {
+            return 'error';
+        }
+        return 'success';
+    }
+
+
+
+
     public function getDesignationById($id) {
         $sql = $this->conn->prepare("SELECT designation.Id, department.Name AS Department, designation.Name as Designation, designation.CreatedAt, designation.DeletedAt FROM `designation` JOIN department ON designation.DepartmentId = department.Id WHERE designation.Id = ?");
         
--- views/adddesgform.php
+++ views/adddesgform.php
@@ -4,12 +4,14 @@ require_once "../models/designations.php";
 require_once "../config/dbconfig.php";
 $desgAdded = false;
 $duplicate = false;
+$desgUpdate = false;
 $error = false;
 $deptModel = new DeptModel($conn);
 $departments = $deptModel->findAll();
 
 $editDept = "";
 $editDesignation = "";
+$editPk = "";
 
 $editFlag = false;
 
@@ -25,6 +27,7 @@ if (isset($_GET['edit'])) {
 
         $editDept =  $editData['Department'];
         $editDesignation =  $editData['Designation'];
+        $editPk = $editData['Id'];
     }
 
     echo $editData['Department']."<br>";
@@ -38,20 +41,54 @@ if (isset($_POST['submitDesg'])) {
     ) {
         exit("invalid input");
     }
+
+    if (!isset($_POST['type'])) {
+        exit("Invalid input!");
+    }
+    
     $desgModel = new DesgModel($conn);
-    $result = $desgModel->insert($_POST);
-    if($result == 'success'){
-        $desgAdded = true;
-    } 
-    elseif($result == 'duplicate') {
-        $duplicate = true;
+
+    if (trim($_POST['type']) == "create") {
+        $result = $desgModel->insert($_POST);
+        if($result == 'success'){
+            $desgAdded = true;
+        } 
+        elseif($result == 'duplicate') {
+            $duplicate = true;
+        }
+        else {
+            $error = true;
+        }
+        echo "create req received!";
+
+    }
+    else if (trim($_POST['type']) == "edit") {
+        echo "Edit req received!<br>";
+        echo $_POST['id'];
+
+        $result = $desgModel->update($_POST);
+        if($result == 'success'){
+            $desgUpdate = true;
+        } 
+        else {
+            $error = true;
+        }
     }
     else {
-        $error = true;
+        echo "no req received!";
+
     }
+
 }
 ?>
-<h2 class="ps-2">Add Desgination</h2>
+<h2 class="ps-2">
+    <?php if (isset($_GET['edit'])): ?>
+        Edit
+    <?php else: ?>
+    Add
+    <?php endif; ?>
+     Designation
+</h2>
 <hr>
 <div class="row justify-content-center">
     <div class="col-12 col-md-6 align-self-center">
@@ -67,11 +104,26 @@ if (isset($_POST['submitDesg'])) {
         </div>
         <?php elseif($error): ?>
         <div class="alert alert-danger alert-dismissible" role="alert">
-            Designation cannot be added due to some error/invalid entry!
+            Some error in processing the request! Try again later.
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        <?php elseif($desgUpdate): ?>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            Designation updated successfully!
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
         <?php endif; ?>
         <form class="border rounded p-2 border-light" method="POST">
+            <input type="hidden" name="type" value="
+                <?php if(isset($_GET['edit'])): ?>edit
+                <?php else: ?>create<?php endif; ?>
+            ">
+
+            <input type="hidden" name="id" value="
+                <?php if(isset($_GET['edit'])): ?><?= $editPk ?>
+                <?php else: ?> <?php endif; ?>
+            ">
+
             <div class="mb-3">
                 <label for="dept-select" class="col-form-label">Select Department</label>
                 <select name="DepartmentId" id="dept-select" class="select2 form-control" data-allow-clear="true">

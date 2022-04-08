0aee9c9abf23a200b6b5f676d02427ddcaba5d71
sachin-jangir <99168247+sachin-jangir@users.noreply.github.com>
2022-04-08 11:05:37 +0530

    update

--- views/desgtable.php
+++ views/desgtable.php
@@ -44,7 +44,7 @@
             <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i
                 class="bx bx-dots-vertical-rounded"></i></button>
             <div class="dropdown-menu">
-              <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-edit-alt me-2"></i>Edit</a>
+              <a class="dropdown-item" href="/addDesg.php?edit=<?= $row['Id'] ?>"><i class="bx bx-edit-alt me-2"></i>Edit</a>
               <a class="dropdown-item" href="javascript:void(0);"><i class="bx bx-trash me-2"></i>Delete</a>
             </div>
           </div>
